45668972ec6aff1c77824dfbad31760100d509db
Joshiki <82230889+Joshikinc@users.noreply.github.com>
2024-02-23 17:09:23 +0600

    Add files via upload

---
+++ Lab2_2/src/main/java/org/example/Game.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.Getter;
+public class Game {
+    @Getter private String name;        //Название игры
+    @Getter private String genre;       //Жанр
+    @Getter private String platform;    //Платформа
+    @Getter private float year;         //Год
+    @Getter private float price;        //Цена
+
+    public Game(String name, String genre, String platform, float year, float price) {
+        this.name = name;
+        this.genre = genre;
+        this.platform = platform;
+        this.year = year;
+        this.price = price;
+    }
+}
